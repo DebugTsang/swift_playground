7e7fc11c55a3fecdcbb26bde3d222b1469f6e29a
Calvin Tsang <debug.tsang@gmail.com>
2016-07-09 15:50:02 +0800

    Initial Commit

---
+++ DataStructure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataStructure
+//
+//  Created by Calvin Tsang on 9/7/2016.
+//  Copyright © 2016 Calvin Tsang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
